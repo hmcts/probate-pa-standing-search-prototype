c7705c172539529a95f811549d5c72651c14f5d1
Rob Armes <robarmes@hotmail.com>
2018-11-06 15:01:31 +0000

    payment up[dated

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Standing search',
+  serviceName: 'Get notified when a grant of probate is issued',
 
   // Default port that prototype runs on
   port: '3000',
